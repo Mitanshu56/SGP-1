1d19f504d80db3bf77c7d1259ef0223159a0e369
Mitanshu56 <148608396+Mitanshu56@users.noreply.github.com>
2024-09-24 22:57:31 +0530

    Update home_1.js

--- Home page/home_1.js
+++ Home page/home_1.js
@@ -1,1 +1,14 @@
 
+// Swiper initialization
+let swiper = new Swiper(".mySwiper", {
+  loop:true,
+  autoplay:{
+      delay :3000,
+      disableOnInteraction:false,
+  },
+  speed :1000,
+  navigation:{
+      nextEl :".swiper-button-next",
+      prevEl :".swiper-button-prev",
+  },
+});
