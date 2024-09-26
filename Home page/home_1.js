16809f0d331e7b6313d0739dcae030d81a919e3e
Hemil195 <144835154+Hemil195@users.noreply.github.com>
2024-09-26 12:31:29 +0530

    Update home_1.js
    adding swiper and highlight effect

--- Home page/home_1.js
+++ Home page/home_1.js
@@ -1,14 +1,29 @@
-
-// Swiper initialization
 let swiper = new Swiper(".mySwiper", {
-  loop:true,
-  autoplay:{
-      delay :3000,
-      disableOnInteraction:false,
-  },
-  speed :1000,
-  navigation:{
-      nextEl :".swiper-button-next",
-      prevEl :".swiper-button-prev",
-  },
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    speed: 1000,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    on: {
+        slideChange: function () {
+            highlightEventCard(this.realIndex);
+        }
+    }
 });
+
+// Function to highlight the event card based on the current slide index
+function highlightEventCard(slideIndex) {
+    // Remove the highlight from all event cards
+    let eventCards = document.querySelectorAll('.event-card');
+    eventCards.forEach(card => card.classList.remove('highlight-event'));
+
+    // Highlight the corresponding event card
+    if (eventCards[slideIndex]) {
+        eventCards[slideIndex].classList.add('highlight-event');
+    }
+}
